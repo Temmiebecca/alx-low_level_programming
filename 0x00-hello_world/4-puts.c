bb1386b6a540216de8981527da7b75f0caa23a09
Temmiebecca <temmiebecca@gmail.com>
2023-04-21 04:04:53 -0700

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - You write anything without special keys otherwise betty wont run it
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
