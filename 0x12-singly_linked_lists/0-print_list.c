b310c477c73854ca89615ef16edfe6937f628d95
Temmiebecca <temmiebecca@gmail.com>
2023-06-02 03:47:59 -0700

    singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,8 +2,8 @@
 #include "lists.h"
 
 /**
- * print_list -  function that prints all the elements
- * of a linked list.
+ * print_list - function that prints all the elements
+ * of a linked list
  * @h: pointer to the list_t list to print
  *
  * Return: the number of nodes printed
